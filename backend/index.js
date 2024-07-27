fa2646ccdad917fc51d3b511fdf6626d74a950f8
lisarnjs <kwonenseo@gmail.com>
2024-07-27 16:32:15 +0900

    fix: change to private with env

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,10 @@ import cors from "cors";
 const app = express();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "85213mysql#",
-  database: "test",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.use(express.json());
